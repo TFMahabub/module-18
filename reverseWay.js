d359a68a454742670f39eb47aa37d3681c8f900c
TFMahabub <rjmahabub543@gmail.com>
2022-08-02 16:25:57 +0600

    reverseWay done

---
+++ reverseWay.js
@@ -0,0 +1,22 @@
+
+var num = 10;
+
+//reverseWay to find the num-
+while(num >= 1){
+  console.log(num);
+  num--;
+}
+
+
+
+
+var numbers = [20, 40, 80, 90, 150, 30, 50, 40, 70, 35]
+
+var num = numbers.length - 1;
+
+for( i = num; i >= 0; i--){
+
+  // number = numbers[i];
+
+  console.log(numbers[i])
+}
