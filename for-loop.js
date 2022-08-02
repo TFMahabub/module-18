d9eeb3beaea2b5c74eb390ecd0245953185e6ab4
TFMahabub <rjmahabub543@gmail.com>
2022-08-02 11:39:55 +0600

    for loop condition

---
+++ for-loop.js
@@ -0,0 +1,15 @@
+//A simple array variable diglaration-
+var numbers= [25, 39, 27, 95, 4, 55, 11, 222, 345, 214, 5453, 5425, 5423, 45, 99]
+
+//find numbers array length-
+numbersLength = numbers.length;
+
+//for-loop-advence-
+for(i = 0; i < numbersLength; i++){
+
+  //single-number to arry index-number connection-
+  singleNumber = numbers[i]
+
+  //for print-
+  console.log(singleNumber)
+}
